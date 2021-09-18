a9e09921d2dfb6968878f7f39ebeefbef83efc62
CuteSkyler <70780048+CuteSkyler@users.noreply.github.com>
2021-09-18 02:57:02 +0200

    Add files via upload

---
+++ v2/content/drawings/script.js
@@ -0,0 +1,94 @@
+var tradDrawingsVar = [
+    "https://cdn.discordapp.com/attachments/556796995088547851/700727496588656750/IMG_20200417_161536_687.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/694351799238524968/IMG_20200328_171542_028.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/693203740836102254/IMG_20200327_215500.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/690335593254879846/IMG_20200320_000546_112.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/688508856330223674/IMG_20200311_153906_501.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/688508710347079708/IMG_20200311_231317_714.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/688508632417042433/IMG_20200313_162335_318.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/688508337654071375/IMG_20200314_225956_390.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/686721684992294974/IMG_20200310_001321_434.jpg",
+    "https://pbs.twimg.com/media/ERKFc_cW4AEovZt.jpg",
+    "https://pbs.twimg.com/media/EQxc9QBXYAAW2W2.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/672534568657747988/IMG_20200130_210408.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/662462872793776142/IMG_20200103_014829.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/661609778119835668/IMG_20191231_164231.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/650841063161856031/IMG_20191201_203318.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/650428647798472748/IMG_20191130_210847.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/649343289866190863/IMG_20191127_211705.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/639576155409743902/IMG_20191031_222558.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/635171336934785031/doggy.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/635171332035706921/doggycum.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/633754115322806298/milfk.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/634140638849269760/image47.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/633005085592387692/unknown.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/630876279146807306/boobjob.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/611994118129844225/halfresdrawing.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/601756546774466590/unknown.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/591363451759099908/unknown.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/592459539195101215/unknown.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/722238368003653752/IMG_20200616_013414_358.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/722557803184783391/IMG_20200616_230327_144.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/722944812831080508/IMG_20200618_004354_701.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/743967887844180118/ezgif-5-e4b415b0f2f9.gif",
+    "https://cdn.discordapp.com/attachments/556796995088547851/745412912650387516/IMG_20200819_002309_212.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/759503305977757746/IMG_20200926_215431_869.jpg",
+    "https://cdn.discordapp.com/attachments/733067931612872735/759861812396163092/IMG_20200927_213901_893.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/768561838605467648/gif24fps.gif",
+    "https://cdn.discordapp.com/attachments/556796995088547851/807618571243880458/IMG_20210205_145029_065.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/810191654622461952/IMG_20210212_213605_005.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/810191660155666462/IMG_20210213_003853_393.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/810507173112971294/IMG_20210214_133104_544.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/810583474146508830/IMG_20210214_193054_232.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/811015717246861342/IMG_20210215_162116_877.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/811015724465782784/IMG_20210215_233330_083.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/811381518219739177/IMG_20210216_234052_305.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/836714925035356180/20210427_231807.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/837076148331610112/20210428_231326.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/848638190072233984/20210530_204505.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/848638190360592394/20210530_204524.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/849895584746831922/20210602_195821.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/849895585884012544/20210602_195832.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/849895586392309791/20210603_000342.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/850886690955460628/20210605_224537.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/859562717144285194/20210629_225556.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/859562717396598804/20210629_231625.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/870778568160968714/20210730_231028.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/870778592789934170/20210730_231102.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/870823977474469888/20210731_021649.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/870888359558529034/20210731_062458.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/870888361404018718/20210731_062445.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/871119482368708691/20210731_213643.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/871439493293559859/20210801_170833.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/871880858112507995/20210802_210441.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/872173818179891270/20210803_193649.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/872245538307850250/20210804_001050.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/876499329102262282/20210809_223227.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/877615868954173480/20210818_200733.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/878270707111428156/20210820_150945.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/879436430638727218/20210823_204410.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/879786022874001468/20210824_190702.png",
+    "https://cdn.discordapp.com/attachments/556796995088547851/880213357716705320/20210825_232016.jpg",
+    "https://cdn.discordapp.com/attachments/556796995088547851/885240105000927232/20210908_192149.png"
+]
+
+function randomise(){
+    var ran = Math.floor(Math.random()*tradDrawingsVar.length)
+    var ran2 = Math.floor(Math.random()*tradDrawingsVar.length)
+    if(ran >= tradDrawingsVar.length-12){
+        ran = 59
+    }
+    if(ran2 >= tradDrawingsVar.length-3){
+        ran2 = 68
+    }
+    for(let i = 0; i < 3; i++){
+        document.getElementById('f'+(i+1)).style.backgroundImage = "url('"+tradDrawingsVar[ran2+i]+"')"
+        document.getElementById('af'+(i+1)).href = tradDrawingsVar[ran+i]
+    }
+    for(let i = 0; i < 12; i++){
+        document.getElementById('i'+(i+1)).style.backgroundImage = "url('"+tradDrawingsVar[ran+i]+"')"
+        document.getElementById('a'+(i+1)).href = tradDrawingsVar[ran+i]
+    };
+};
+
+randomise()
