35ae7463bc67326b604f442c53d195fa60725027
CuteSkyler <70780048+CuteSkyler@users.noreply.github.com>
2021-09-21 23:52:54 +0200

    Add files via upload

---
+++ v2/script.js
@@ -0,0 +1,6 @@
+function fuckyouIframe(){
+    document.getElementById("discordfuckyou").width = 0.28125*window.innerHeight
+    document.getElementById("discordfuckyou").height = 0.5*window.innerHeight
+}
+
+fuckyouIframe()
