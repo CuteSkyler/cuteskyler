e9b0c3356f9e893bc28e4b291bad5baf7663fa14
CuteSkyler <70780048+CuteSkyler@users.noreply.github.com>
2022-03-13 14:40:01 +0100

    Update main.js

--- growthstory/main.js
+++ growthstory/main.js
@@ -133,13 +133,13 @@ function changeStats(muscChange, intlChange, mneyChange, wghtChange, engrChange)
     wghtChange = wghtChange * multiplier;
     intlChange = intlChange * multiplier;
     engrChange = engrChange / (1+multiplier/10);
-    mneyChange = Math.floor(mneyChange * (intelligence / 100));
+    mneyChange = mneyChange * (intelligence / 100);
     if(muscle + muscChange <= 10){muscle = 10;}
     else if(muscle + muscChange > 10){muscle += muscChange;};
     if(intelligence + intlChange <= 50){intelligence = 50;}
     else if(intelligence + intlChange > 50){intelligence += intlChange;};
     if(money + mneyChange <= 0){money = 0;}
-    else if(money + mneyChange > 0){money += mneyChange;};
+    else if(money + mneyChange > 0){money += Math.floor(mneyChange);};
     if(weight + wghtChange <= muscle+10){weight = muscle+10;}
     else if(weight + wghtChange > muscle+10){weight += wghtChange;};
     if(energy + engrChange <= 0){energy = 0;}
