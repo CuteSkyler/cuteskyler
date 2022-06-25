8b509618cf3217ccaa39b1755d7bbf7d49a8515f
CuteSkyler <70780048+CuteSkyler@users.noreply.github.com>
2022-06-25 21:26:49 +0200

    Add files via upload

--- prices/main.js
+++ prices/main.js
@@ -1,8 +1,10 @@
 const sheet = document.querySelector('div#sheet div#main');
 const gals = document.querySelectorAll('div#sheet div.gal');
+const grid = document.querySelector('div#sheet div#grid');
 
 const FACTOR = 50;
 const SHEETFACTOR = 25;
+const GRIDFACTOR = 9;
 
 function setTurn(x, y){
     let width = window.innerWidth;
@@ -23,6 +25,7 @@ function setTurn(x, y){
     sheet.style.transform = `rotateY(${calcX/SHEETFACTOR}deg) rotateX(${calcY*-1/SHEETFACTOR}deg)`;
     gals[0].style.transform = `rotateY(${calcX*-1/FACTOR}deg) rotateX(${calcY/FACTOR}deg) translateX(-10%) translateY(10%) scale(-1.05, 1.05)`;
     gals[1].style.transform = `rotateY(${calcX*-1/FACTOR}deg) rotateX(${calcY/FACTOR}deg) translateX(10%) translateY(10%) scale(1.05)`;
+    grid.style.transform= `rotateX(${calcY/10+80}deg) translateX(-25%)`
 };
 
 document.body.addEventListener('mousemove', e=>{
